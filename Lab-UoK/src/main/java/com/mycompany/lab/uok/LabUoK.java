62cd59b6a828088da47f10b0141d636c3571f8c4
SithikaJayawickrama <sithalankarage306@gmail.com>
2026-04-14 20:04:32 +0530

    Add Lab-UoK Maven project and main class
    Create a new Maven project 'Lab-UoK' with a pom.xml (groupId com.mycompany, artifactId Lab-UoK, packaging jar, maven.compiler.release=23 and exec.mainClass configured). Add a simple LabUoK.java in com.mycompany.lab.uok containing a main method that prints "Hello World!".

---
+++ Lab-UoK/src/main/java/com/mycompany/lab/uok/LabUoK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab.uok;
+
+/**
+ *
+ * @author HP
+ */
+public class LabUoK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
